3612f26b529f6955b1eb013f8e9e54e82af1ee72
zulfahmidev <developer.zulfahmi@gmail.com>
2025-08-08 15:17:24 +0700

    [FEAT] Perbaikan CORS

--- cargo_app/settings.py
+++ cargo_app/settings.py
@@ -45,9 +45,11 @@ INSTALLED_APPS = [
     'calculator',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -158,6 +160,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
 }
 
@@ -168,3 +171,5 @@ CSRF_TRUSTED_ORIGINS = [
     'http://cargo-app.zulfahmidev.com',
     'https://cargo-app.zulfahmidev.com',
 ]
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
